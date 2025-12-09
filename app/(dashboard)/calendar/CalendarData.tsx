adb0c22d895810037f30cf711d949fcb315f7389
oyewole timilehin <oyewoleoluwatimilehin2@gmail.com>
2025-12-09 12:25:11 -0800

    Add CalendarToolbar and EventCard components; integrate them into the calendar page

--- app/(dashboard)/calendar/CalendarData.tsx
+++ app/(dashboard)/calendar/CalendarData.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export interface CalendarEvent {
   id: number;
   title: string;
@@ -25,3 +27,33 @@ export const CalendarEvents: CalendarEvent[] = [
     color: "bg-yellow-200 text-yellow-800",
   },
 ];
+
+export function CalendarToolbar({ label, onView }) {
+  return (
+    <div className="flex justify-between items-center mb-5">
+      <h2 className="font-semibold text-lg">{label}</h2>
+
+      <div className="flex bg-gray-100 rounded-full">
+        {["month", "week", "day"].map((view) => (
+          <button
+            key={view}
+            onClick={() => onView(view)}
+            className="px-4 py-1.5 text-sm font-medium rounded-full hover:bg-white"
+          >
+            {view.charAt(0).toUpperCase() + view.slice(1)}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export function EventCard({ event }: any) {
+  return (
+    <div
+      className={`rounded-md text-xs px-2 py-1 font-medium shadow-sm ${event.color}`}
+    >
+      {event.title}
+    </div>
+  );
+}
--- app/(dashboard)/calendar/page.tsx
+++ app/(dashboard)/calendar/page.tsx
@@ -1,9 +1,33 @@
 "use client";
+import { Calendar, Views, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import { CalendarEvents, CalendarToolbar, EventCard } from "./CalendarData";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-6">
+      {/* Main Calendar */}
+      <div className="col-span-3 bg-white rounded-xl shadow-md p-5 ">
+        <Calendar
+          localizer={localizer}
+          events={CalendarEvents}
+          startAccessor="start"
+          endAccessor="end"
+          views={["month", "week", "day"]}
+          components={{
+            event: EventCard,
+            toolbar: CalendarToolbar,
+          }}
+          style={{ height: 800 }}
+        />
+      </div>
+      {/* sidebar */}
+      <div></div>
+    </div>
+  );
 };
 
 export default page;
